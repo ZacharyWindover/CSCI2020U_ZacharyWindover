88be5066b857f911d9772d3e220f2ff924a6e069
Zachary Windover <zachary.windover@ontariotechu.net>
2021-02-22 16:00:20 -0500

    Lab 04

--- lab03/src/main/java/CSCI2020U/lab03/CSVDemo.java
+++ lab03/src/main/java/CSCI2020U/lab03/CSVDemo.java
@@ -4,7 +4,7 @@ import org.apache.commons.csv.*;
 public class CSVDemo {
 	public static void main (String[] args) {
 		try {
-			Reader in = new FileReader("data.cav");
+			Reader in = new FileReader("data.csv");
 			Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("First Name", "Last Name", "SID", "Grade").parse(in);
 			
 			for (CSVRecord record: records) {
