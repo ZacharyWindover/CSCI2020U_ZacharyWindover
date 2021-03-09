311e1018862b242e5ea3324968a84e0242d96246
ZacharyWindover <71024596+ZacharyWindover@users.noreply.github.com>
2021-03-08 21:52:21 -0500

    Update Main.java

--- Spam Detection System/src/SpamDetection/Main.java
+++ Spam Detection System/src/SpamDetection/Main.java
@@ -1,44 +1,29 @@
-<?import javafx.geometry.Insets?>
-<?import javafx.scene.layout.GridPane?>
+package SpamDetection;
 
-<?import javafx.scene.control.Button?>
-<?import javafx.scene.control.Label?>
-<?import javafx.scene.control.TableView?>
-<?import javafx.scene.control.TableColumn?>
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-<GridPane fx:controller="SpamDetection.Controller"
-          xmlns:fx="http://javafx.com/fxml" alignment="center" hgap="10" vgap="10" style="-fx-background-color: #0000FF;">
+public class Main extends Application {
 
-    <Label text="Spam Detection System" GridPane.columnIndex="0" GridPane.rowIndex="0" style=
-            "-fx-font-size: 48 arial;
-            -fx-font-weight: bold;
-            -fx-color: #FFFFFF;
-            -fx-background-color: #FFFFFF;
-            -fx-alignment: center;"/>
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Spam Detection System");
 
-    <TableView fx:id="tableView"  GridPane.columnIndex="0" GridPane.rowIndex="1" maxHeight="720" maxWidth="1280" editable="false" minHeight="480" minWidth="560" prefHeight="480" prefWidth="560">
-       <columns>
-           <TableColumn fx:id="fileColumn" text="Student Number"></TableColumn>
-           <TableColumn fx:id="actualClassColumn" text="Actual Class"></TableColumn>
-           <TableColumn fx:id="spamProbabilityColumn" text="Spam Probability"></TableColumn>
-           <TableColumn fx:id="classGuessColumn" text="Calculated Class"></TableColumn>
+        Scene scene = new Scene(root, 1280, 720);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-       </columns>
+    }
 
-    </TableView>
-
-    <Label fx:id="accuracyLabel" text="" GridPane.ColumnIndex="0" GridPane.RowIndex="2" style=
-            "-fx-font-size: 24 arial;
-            -fx-font-weight: bold;
-            -fx-color: #FFFFFF;
-            -fx-background-color: #FFFFFF;
-            -fx-alignment: center;"/>
-    
-    <Label fx:id="precisionLabel" text="" GridPane.ColumnIndex="0" GridPane.RowIndex="3" style=
-            "-fx-font-size: 24 arial;
-            -fx-font-weight: bold;
-            -fx-color: #FFFFFF;
-            -fx-background-color: #FFFFFF;
-            -fx-alignment: center;"/>
-
-</GridPane>
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
